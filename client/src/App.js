d37cc7572103b2dab7bf2cbefd362ae32636e4bf
eqzhou81 <84222539+eqzhou81@users.noreply.github.com>
2021-08-22 01:07:54 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,28 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import UserDashBoard from './pages/UserDashboard';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <Router>
       <div className="App">
         <div className="navbar-extended">
-          <Navbar />
+          <Navbar loggedIn={loggedIn} />
         </div>
         <Switch>
           <Route exact path="/">
-            <Homepage />
+            <Homepage loggedIn={loggedIn} />
           </Route>
           <Route path="/userinfo">
             <UserDashBoard />
+          </Route>
+          <Route path="/login">
+            <Login setLoggedIn={setLoggedIn} />
           </Route>
         </Switch>
       </div>
