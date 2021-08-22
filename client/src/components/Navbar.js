a42be4e226a563382fc7104a2286fa4fc17421df
eqzhou81 <84222539+eqzhou81@users.noreply.github.com>
2021-08-22 01:11:05 -0700

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
 import logo from "../images/habifix_1.png";
 
-const Navbar = () => {
+
+
+const Navbar = ({ loggedIn }) => {
   return (
     <nav className="navbar">
       <img className="logo" src={logo} alt="logo-img"/>
       <h1>HabiFix</h1>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/userinfo">Your Stats</Link>
+        {loggedIn ? (
+          <Link to="/userinfo">Your stats</Link>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
       </div>
     </nav>
   );
