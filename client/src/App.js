c907f838b4ccc0ae34720a27fde0646f535b2193
eqzhou81 <84222539+eqzhou81@users.noreply.github.com>
2021-08-22 00:57:33 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,23 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
-import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import UserDashBoard from './pages/UserDashboard';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <Router>
       <div className="App">
         <div className="navbar-extended">
-          <Navbar loggedIn={loggedIn} />
+          <Navbar />
         </div>
         <Switch>
           <Route exact path="/">
-            <Homepage loggedIn={loggedIn} />
+            <Homepage />
           </Route>
           <Route path="/userinfo">
             <UserDashBoard />
-          </Route>
-          <Route path="/login">
-            <Login setLoggedIn={setLoggedIn} />
           </Route>
         </Switch>
       </div>
