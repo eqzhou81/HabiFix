50c81381bbc1dcc4d1266cad5b4fa16f5b91e866
eqzhou81 <84222539+eqzhou81@users.noreply.github.com>
2021-08-22 00:57:58 -0700

    Add files via upload

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,14 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from "../images/habifix_1.png";
 
-const Navbar = ({ loggedIn }) => {
+const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Habifix</h1>
+      <img className="logo" src={logo} alt="logo-img"/>
+      <h1>HabiFix</h1>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        {loggedIn ? (
-          <Link to="/userinfo">Your stats</Link>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
+        <Link to="/userinfo">Your Stats</Link>
       </div>
     </nav>
   );
